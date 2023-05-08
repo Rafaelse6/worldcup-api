41ba6aefafcd6e9254b0901f29353a7ed2ec671a
Rafael Santos <rafa.05125@gmail.com>
2023-05-08 11:26:39 -0300

    POST, DELETE teams

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 const app = express();
+app.use(express.json());
 
 //Mock
 const selecoes = [
@@ -15,15 +16,23 @@ const selecoes = [
   },
   {
     id: 3,
-    selecao: "Sérvia",
+    selecao: "Camarões",
     grupo: "G",
   },
   {
     id: 4,
-    selecao: "Camarões",
+    selecao: "Sérvia",
     grupo: "G",
   },
 ];
+
+function buscarSelecaoPorId(id) {
+  return selecoes.filter((selecao) => selecao.id == id);
+}
+
+function buscarIndexSelecao(id) {
+  return selecoes.findIndex((selecao) => selecao.id == id);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -31,6 +40,21 @@ app.get("/", (req, res) => {
 
 app.get("/selecoes", (req, res) => {
   res.status(200).send(selecoes);
+});
+
+app.get("/selecoes/:id", (req, res) => {
+  res.json(buscarSelecaoPorId(req.params.id));
+});
+
+app.post("/selecoes", (req, res) => {
+  selecoes.push(req.body);
+  res.status(201).send("Seleção cadastrada com sucesso");
+});
+
+app.delete("/selecoes/:id", (req, res) => {
+  let index = buscarIndexSelecao(req.params.id);
+  selecoes.splice(index, 1);
+  res.send(`Seleção com id ${req.params.id} excluída com sucesso`);
 });
 
 export default app;
