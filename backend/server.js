a49a7129bdfe3ef845009a48ba46f86afcb7e654
Rafael Santos <rafa.05125@gmail.com>
2023-05-08 17:13:21 -0300

    MySQL connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 import app from "./src/App.js";
+import conexao from "./infra/conexao.js";
+
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+conexao.connect((erro) => {
+  if (erro) {
+    console.log(erro);
+  } else {
+    app.listen(PORT, () => {
+      console.log(`Example app listening on port ${PORT}`);
+    });
+
+    console.log("Conexão realizada com sucesso");
+  }
 });
