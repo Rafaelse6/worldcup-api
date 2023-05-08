ac6c4386b5245754fa8e2b386c18bebc8d0e9cad
Rafael Santos <rafa.05125@gmail.com>
2023-05-08 16:08:13 -0300

    PUT teams

--- backend/src/app.js
+++ backend/src/app.js
@@ -34,6 +34,11 @@ function buscarIndexSelecao(id) {
   return selecoes.findIndex((selecao) => selecao.id == id);
 }
 
+app.post("/selecoes", (req, res) => {
+  selecoes.push(req.body);
+  res.status(201).send("Seleção cadastrada com sucesso");
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -46,9 +51,11 @@ app.get("/selecoes/:id", (req, res) => {
   res.json(buscarSelecaoPorId(req.params.id));
 });
 
-app.post("/selecoes", (req, res) => {
-  selecoes.push(req.body);
-  res.status(201).send("Seleção cadastrada com sucesso");
+app.put("/selecoes/:id", (req, res) => {
+  let index = buscarIndexSelecao(req.params.id);
+  selecoes[index].selecao = req.body.selecao;
+  selecoes[index].grupo = req.body.grupo;
+  res.json(selecoes);
 });
 
 app.delete("/selecoes/:id", (req, res) => {
