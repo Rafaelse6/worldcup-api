fa4db7bdc2edbd36b8c9386aedf4d8f548bb9f1e
Rafael Santos <rafa.05125@gmail.com>
2023-05-09 14:24:29 -0300

    SQL queries

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,7 @@
 import app from "./src/App.js";
-import conexao from "./infra/conexao.js";
 
 const PORT = 3000;
 
-conexao.connect((erro) => {
-  if (erro) {
-    console.log(erro);
-  } else {
-    app.listen(PORT, () => {
-      console.log(`Example app listening on port ${PORT}`);
-    });
-
-    console.log("Conexão realizada com sucesso");
-  }
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,30 +1,9 @@
 import express from "express";
-const app = express();
-app.use(express.json());
+import conexao from "../infra/conexao.js";
 
-//Mock
-const selecoes = [
-  {
-    id: 1,
-    selecao: "Brasil",
-    grupo: "G",
-  },
-  {
-    id: 2,
-    selecao: "Suíça",
-    grupo: "G",
-  },
-  {
-    id: 3,
-    selecao: "Camarões",
-    grupo: "G",
-  },
-  {
-    id: 4,
-    selecao: "Sérvia",
-    grupo: "G",
-  },
-];
+const app = express();
+
+app.use(express.json());
 
 function buscarSelecaoPorId(id) {
   return selecoes.filter((selecao) => selecao.id == id);
@@ -35,33 +14,69 @@ function buscarIndexSelecao(id) {
 }
 
 app.post("/selecoes", (req, res) => {
-  selecoes.push(req.body);
-  res.status(201).send("Seleção cadastrada com sucesso");
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
+  const selecao = req.body;
+  const sql = "INSERT INTO selecoes SET ?";
+  conexao.query(sql, selecao, (erro, result) => {
+    if (erro) {
+      console.log(erro);
+      res.status(400).json({ erro: `${erro}` });
+    } else {
+      res.status(201).json(result);
+    }
+  });
 });
 
 app.get("/selecoes", (req, res) => {
-  res.status(200).send(selecoes);
+  const sql = "SELECT * FROM selecoes";
+  conexao.query(sql, (erro, result) => {
+    if (erro) {
+      console.log(erro);
+      res.status(404).json({ erro: `${erro}` });
+    } else {
+      res.status(200).json(result);
+    }
+  });
 });
 
 app.get("/selecoes/:id", (req, res) => {
-  res.json(buscarSelecaoPorId(req.params.id));
+  const id = req.params.id;
+  const sql = "SELECT * FROM selecoes WHERE id=?";
+  conexao.query(sql, id, (erro, result) => {
+    const linha = result[0];
+    if (erro) {
+      console.log(erro);
+      res.status(404).json({ erro: `${erro}` });
+    } else {
+      res.status(200).json(linha);
+    }
+  });
 });
 
 app.put("/selecoes/:id", (req, res) => {
-  let index = buscarIndexSelecao(req.params.id);
-  selecoes[index].selecao = req.body.selecao;
-  selecoes[index].grupo = req.body.grupo;
-  res.json(selecoes);
+  const id = req.params.id;
+  const selecao = req.body;
+  const sql = "UPDATE selecoes SET ? WHERE id=?";
+  conexao.query(sql, [selecao, id], (erro, result) => {
+    if (erro) {
+      console.log(erro);
+      res.status(400).json({ erro: `${erro}` });
+    } else {
+      res.status(200).json(result);
+    }
+  });
 });
 
 app.delete("/selecoes/:id", (req, res) => {
-  let index = buscarIndexSelecao(req.params.id);
-  selecoes.splice(index, 1);
-  res.send(`Seleção com id ${req.params.id} excluída com sucesso`);
+  const id = req.params.id;
+  const sql = "DELETE FROM selecoes WHERE id=?";
+  conexao.query(sql, id, (erro, result) => {
+    if (erro) {
+      console.log(erro);
+      res.status(404).json({ erro: `${erro}` });
+    } else {
+      res.status(200).json(result);
+    }
+  });
 });
 
 export default app;
